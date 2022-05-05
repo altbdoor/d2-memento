2e0fe45c00c0440e9bdb3f01f89b6de8e19bb995
altbdoor <lancersupraskyline@gmail.com>
2022-05-05 20:21:08 +0800

    the power of powershell [skip ci]

--- script/fetch-csv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# thanks /u/TheLastJoaquin
-# https://docs.google.com/spreadsheets/d/1Msr7Vdqfa6MlMXYSlERAnbTwEr4Fd7rVe2uvxaPjo14/edit#gid=0
-
-sheet='1Msr7Vdqfa6MlMXYSlERAnbTwEr4Fd7rVe2uvxaPjo14'
-mkdir -p output
-curl -sL -o 'output/input.csv' "https://docs.google.com/spreadsheets/d/${sheet}/export?format=csv&gid=0"
---
+++ script/setup.ps1
@@ -0,0 +1,104 @@
+#!/usr/bin/env pwsh
+
+$MyInvocation.MyCommand.Path | Split-Path | Push-Location
+
+# path to file.exe to check for png
+# ========================================
+$pathToFileExe = $args[0]
+if (!$pathToFileExe) {
+    throw 'please provide path to file.exe'
+}
+
+# thanks /u/TheLastJoaquin
+# https://docs.google.com/spreadsheets/d/1Msr7Vdqfa6MlMXYSlERAnbTwEr4Fd7rVe2uvxaPjo14/edit#gid=0
+
+# fetching csv data
+# ========================================
+$sheet = '1Msr7Vdqfa6MlMXYSlERAnbTwEr4Fd7rVe2uvxaPjo14'
+New-Item -ItemType Directory -Force -Path 'output'
+Invoke-WebRequest -OutFile 'output/input.csv' `
+    -Uri "https://docs.google.com/spreadsheets/d/${sheet}/export?format=csv&gid=0"
+
+# parsing csv into meaningful data
+# ========================================
+$headers = @('Group', 'Weapon', 'Gambit', 'Spacer1', 'Trials', 'Spacer2', 'Nightfall')
+$csv = Import-Csv -Path 'output/input.csv' -Header $headers
+$csv = $csv | Select-Object -Skip 1
+
+$data = @()
+foreach ($line in $csv) {
+    $datum = @{
+        group     = $line.Group.Trim();
+        weapon    = $line.Weapon.Trim();
+        gambit    = $line.Gambit.Trim();
+        trials    = $line.Trials.Trim();
+        nightfall = $line.Nightfall.Trim();
+        slug      = '';
+    }
+
+    if (($datum.gambit -eq '') -and ($datum.trials -eq '') -and ($datum.nightfall -eq '')) {
+        break
+    }
+
+    if ($datum.group -eq '') {
+        $datum.group = $data[-1].group
+    }
+
+    $datum.slug = $datum.weapon.ToLower() -replace '[^A-z0-9]', ''
+    $data += @(, $datum)
+}
+
+# cleaning image urls
+# ========================================
+$activities = @('gambit', 'trials', 'nightfall')
+foreach ($datum in $data) {
+    foreach ($act in $activities) {
+        $imageUrl = $datum.($act)
+
+        if ($imageUrl -match 'imgur.com/a/') {
+            $html = (Invoke-WebRequest -Uri $imageUrl).Content
+            $imageUrl = $html | Select-String -Pattern '"https://i.imgur.com/.+?"' | ForEach-Object { $_.Matches.Value } | Select-Object -First 1
+            $imageUrl = $imageUrl -replace '"', ''
+        }
+        elseif ($imageUrl -match 'drive.google.com') {
+            $driveId = $imageUrl.Split('/')[5]
+            $imageUrl = "https://docs.google.com/uc?export=download&id=${driveId}"
+        }
+
+        $datum.($act) = $imageUrl
+    }
+}
+
+# fetching images
+# ========================================
+New-Item -ItemType Directory -Force -Path 'images'
+
+foreach ($datum in $data) {
+    foreach ($act in $activities) {
+        $imageUrl = $datum.($act)
+        $filename = "$($datum.slug)-$($act.ToLower())"
+        $findFilename = Get-ChildItem -Path 'images' -Name -Include "${filename}*"
+
+        if ($findFilename) {
+            Write-Output "images/${findFilename} already exist"
+        }
+        else {
+            Invoke-WebRequest -Uri "${imageUrl}" -OutFile "images/${filename}.jpg"
+            $findFilename = "${filename}.jpg"
+
+            $fileMetadata = & "${pathToFileExe}" "images/${filename}.jpg"
+            if ($fileMetadata -match 'PNG image data') {
+                Move-Item -Path "images/${filename}.jpg" -Destination "images/${filename}.png"
+                $findFilename = "${filename}.png"
+            }
+
+            Write-Output "downloaded images/${findFilename} from ${imageUrl}"
+        }
+
+        $datum.($act) = $findFilename
+    }
+}
+
+# output to json
+# ========================================
+ConvertTo-Json -InputObject $data -Depth 100 | Out-File -FilePath "output/data.json"
